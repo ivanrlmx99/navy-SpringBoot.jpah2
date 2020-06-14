6f6bfa57196841222d875e612e67d86ea6d13e9e
ivanrlmx99 <ivanlaureano77@gmail.com>
2020-06-14 16:24:50 -0500

    plantilla jpa2

---
+++ src/main/java/com/ivan/mx/navy/jpa/app/SpringBootJpaDataApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.mx.navy.jpa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaDataApplication.class, args);
+    }
+
+}
